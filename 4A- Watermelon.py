4134a7978b897977226d8d001dca86352d2da6e0
Shafia Afrin Abritty <44696619+Abritty@users.noreply.github.com>
2020-07-25 20:45:19 +0600

    Add files via upload

---
+++ 4A- Watermelon.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 20:37:01 2020
+
+@author: Shafiabi
+"""
+
+w = int(input())
+ 
+if (w<=2):
+    print("NO")
+elif (w%2 == 0):
+    print("YES")
+else :
+    print("No")
